ce6f13419f7c8801a10b553b961f5cb8605dee82
“Dardo_Uriel” <urielherrera412@gmail.com>
2023-06-30 16:32:07 -0300

    vadenuevo

---
+++ Ecommmerce/src/test/java/com/proyectoEcommerce/Ecommmerce/EcommmerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoEcommerce.Ecommmerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommmerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
